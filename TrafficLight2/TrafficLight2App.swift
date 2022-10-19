1cd5ece636b49d557a21f23dfa8b9ed12948f5c6
Albina <olifirenko.albina@gmail.com>
2022-10-19 08:32:52 +0300

    Initial Commit

---
+++ TrafficLight2/TrafficLight2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight2App.swift
+//  TrafficLight2
+//
+//  Created by mac on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
