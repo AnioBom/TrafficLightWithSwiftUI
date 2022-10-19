d0b058066c592a3093da07ea249acaf9a3ddf77e
Albina <olifirenko.albina@gmail.com>
2022-10-19 14:24:32 +0300

    configuration button

--- TrafficLight2/ContentView.swift
+++ TrafficLight2/ContentView.swift
@@ -7,10 +7,30 @@
 
 import SwiftUI
 
+struct ButtonTapped: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+        Text("Start")
+            .font(.title)
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(20)
+            .shadow(color: Color.blue.opacity(0.3), radius: 20, x: 0, y: 10)
+        Group {
+            if configuration.isPressed {
+                Text("Next")
+            } else {
+                Text("Start")
+            }
+        }
+    }
+}
+
+
 struct ContentView: View {
     
-    @State var lightIsOff = false
-    @State var nameButton = "Start"
 
     var body: some View {
         VStack {
@@ -18,15 +38,15 @@ struct ContentView: View {
             YellowCircle(color: .yellow)
             GreenCircle(color: .green)
             Spacer()
-            Button {
-                if nameButton {
-                    Button(
-                }
+            Button(action: {
+                print("")
+            })
+            .buttonStyle(ButtonTapped())
+            
         }
         .padding(20)
     }
 }
-
 
 
 struct ContentView_Previews: PreviewProvider {
